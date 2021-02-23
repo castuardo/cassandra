1d26c3d5a9d566570351c448d5326ec6067147e0
castuardo@uchicago.edu <castuardo@uchicago.edu>
2021-02-23 08:31:18 -0800

    comments

---
+++ git-push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## this branch has to be pushed in a special way since there is a tag with
+## the same name...
+
+git push origin refs/heads/cassandra-3.0.18:refs/heads/cassandra-3.0.18
+
